c21982b0bf6edc0d16e63b544dcc49f0223570fd
Josseph Cortez <jcortezmg@gmail.com>
2023-03-29 19:08:16 -0500

    initial project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-modern-personal-website.netlify.app',
+  output: 'static',
+  site: 'https://portfolioapp-srcortez45.vercel.app/',
+  adapter: vercel(),
   integrations: [mdx(), sitemap(), tailwind()]
 });
